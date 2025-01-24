a3cb2f3c147984ce091ee951d96fd4c7b67cc47f
user <user@DESKTOP-S6UUK9H>
2025-01-24 16:51:54 +0900

    push

--- TennisGame_JAVA_PR/src/com/tennis/input/Script.java
+++ TennisGame_JAVA_PR/src/com/tennis/input/Script.java
@@ -1,5 +1,12 @@
 package com.tennis.input;
 
 public class Script {
+	
+	/*
+	 * 입력값 처리하는부분 Scanner 등으로 문자열 반환해주는 부분
+	 * 
+	 * 
+	 * 
+	 */
 
 }
--- TennisGame_JAVA_PR/src/com/tennis/player/Script.java
+++ TennisGame_JAVA_PR/src/com/tennis/player/Script.java
@@ -1,5 +1,7 @@
 package com.tennis.player;
 
 public class Script {
+	
+	// 선수구현부분 
 
 }
